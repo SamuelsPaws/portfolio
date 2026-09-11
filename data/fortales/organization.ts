d24fdd112d0720ff5c5f1009b7fb5601ecaa87e1
Samuel Baquero <samsantbaq@gmail.com>
2026-09-10 23:00:00 -0500

    favicon ico

--- data/fortales/organization.ts
+++ data/fortales/organization.ts
@@ -1,8 +1,10 @@
 const organization = {
     name: 'Fortales',
-    url: 'https://samshowcase.info/',
+    url: 'https://fortal.es',
     email: 'info@fortal.es',
-    phone: '+593984892528'
+    phone: '+593984892528',
+    author: 'Fortales',
+    image: '/opengraph-image.jpg'
 }
 
 export default organization
