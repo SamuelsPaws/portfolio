4449b44f51eebdc4789d6d568cbaac0fc605d29c
Samuel Baquero <samsantbaq@gmail.com>
2026-09-16 01:47:07 -0500

    small fix

--- app/[locale]/portfolio/components/project-card/subcomponents/ImplementationItem.tsx
+++ app/[locale]/portfolio/components/project-card/subcomponents/ImplementationItem.tsx
@@ -6,7 +6,7 @@ const ImplementationItem = ({ text }: Props) => {
     return (
     <div className="
         max-w-full px-3 sm:px-4 py-2 wrap-break-word
-        bg-gray-200 dark:bg-br-gray-600
+        bg-black/10 dark:bg-br-gray-600
         text-secondary
         rounded-full"
     >
